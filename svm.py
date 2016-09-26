0afb4d4967737d8232eb111b983dffddc041a5ce
Mikael Persson <mikaepe@kth.se>
2016-09-26 16:50:46 +0200

    classA, B

--- svm.py
+++ svm.py
@@ -3,4 +3,22 @@ import numpy, pylab, random, math
 from cvxopt.solvers import qp
 from cvxopt.base import matrix
 
+
+classA = [(random.normalvariate(-1.5, 1),
+    random.normalvariate(0.5,1),
+    1)
+    for i in range(5)] + \
+            [(random.normalvariate(1.5, 1),
+            random.normalvariate(0.5, 1),
+            1.0)
+            for i in range(5)]
+
+classB = [(random.normalvariate(0.0, 0.5),
+    random.normalvariate(-0.5, 0.5),
+    -1.0)
+    for i in range(10)]
+
+data = classA + classB
+random.shuffle(data)
+
 # HEEJ
