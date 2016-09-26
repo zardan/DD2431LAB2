310bd7ab86582749812cfeb10c59b3395acc87ae
zardan <sardan@kth.se>
2016-09-26 17:01:01 +0200

    separate generation of data

---
+++ data.py
@@ -0,0 +1,22 @@
+
+# generate data of two classes
+
+def generateDate():
+    classA = [(random.normalvariate(-1.5, 1),
+        random.normalvariate(0.5,1),
+        1)
+        for i in range(5)] + \
+                [(random.normalvariate(1.5, 1),
+                random.normalvariate(0.5, 1),
+                1.0)
+                for i in range(5)]
+
+    classB = [(random.normalvariate(0.0, 0.5),
+        random.normalvariate(-0.5, 0.5),
+        -1.0)
+        for i in range(10)]
+
+    data = classA + classB
+    random.shuffle(data)
+
+    return classA, classB, data
