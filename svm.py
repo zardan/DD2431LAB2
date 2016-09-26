44931df09dc203a38c2b3ded1c086d8c9ee07b07
Mikael Persson <mikaepe@kth.se>
2016-09-26 17:40:23 +0200

    hej

--- svm.py
+++ svm.py
@@ -5,8 +5,12 @@ from cvxopt.base import matrix
 
 from data import generateData
 from plot import plotFun
+from kernels import *
 
 cA,cB,d = generateData()
+
+#linK = lin_ker(cA...nat...)
+print cA
 
 plotFun(cA,cB)
 
