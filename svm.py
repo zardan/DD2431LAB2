e5ff4d8efa89aa2e7dc89d90eecf1a32dad849eb
zardan <sardan@kth.se>
2016-09-29 12:01:27 +0200

    qp solved

--- svm.py
+++ svm.py
@@ -7,13 +7,6 @@ from data import generateData
 from plot import plotFun
 from kernels import *
 
-cA,cB,d = generateData()
-
-#linK = lin_ker(cA...nat...)
-print cA
-
-plotFun(cA,cB)
-
 def Pmatrix(data, kernel):
     N = len(data)
     P = numpy.zeros(shape=(N,N))
@@ -22,5 +15,24 @@ def Pmatrix(data, kernel):
             P[i][j] = data[i][2]*data[j][2]*kernel(data[i],data[j])
     return P
 
-P = Pmatrix(d,lin_ker)
-print P
+cA,cB,d = generateData()
+#plotFun(cA,cB)
+
+P = Pmatrix(d, lin_ker)
+q = -numpy.ones(len(d))
+c = 1
+G = -numpy.identity(len(d))
+G = numpy.append(G,-G,0)
+h = numpy.zeros(len(d))
+h = numpy.append(h,c*numpy.ones(len(d)),0)
+
+res = qp(matrix(P), matrix(q), matrix(G), matrix(h))
+alpha = list(res['x'])
+print 'alpha =', alpha
+print 'max alpha =', max(alpha)
+
+array_np = numpy.asarray(alpha)
+zeroind = array_np < 1e-5
+array_np[zeroind] = 0
+a = array_np
+print 'a = ', a
