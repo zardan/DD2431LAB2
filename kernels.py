fb2db5d65a77a3a20175e83eebbdce0841d13417
Mikael Persson <mikaepe@kth.se>
2016-09-29 10:39:59 +0200

    comment

--- kernels.py
+++ kernels.py
@@ -1,6 +1,8 @@
 import numpy, pylab, random, math
 
 # a linear kernel functionn (2D)
+# takes two lists/arrays as vectors
+# and computes K(x,y)
 def lin_ker(x,y):
     product = 0
     for i in range(len(x)-1):
