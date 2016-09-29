e00185df51cc6020b19dfd415c45ce158f6f0088
Mikael Persson <mikaepe@kth.se>
2016-09-29 12:41:06 +0200

    list comprehension

--- svm.py
+++ svm.py
@@ -16,7 +16,9 @@ def Pmatrix(data, kernel):
     return P
 
 cA,cB,d = generateData()
-#plotFun(cA,cB)
+
+#kernel = input('Kernel:\n')
+#P = Pmatrix(d, kernel)
 
 P = Pmatrix(d, lin_ker)
 q = -numpy.ones(len(d))
@@ -28,11 +30,16 @@ h = numpy.append(h,c*numpy.ones(len(d)),0)
 
 res = qp(matrix(P), matrix(q), matrix(G), matrix(h))
 alpha = list(res['x'])
-print 'alpha =', alpha
-print 'max alpha =', max(alpha)
 
-array_np = numpy.asarray(alpha)
-zeroind = array_np < 1e-5
-array_np[zeroind] = 0
-a = array_np
-print 'a = ', a
+th = 1e-5
+# Set ~0 values to exactly 0 in alpha
+alpha = [x if x >= th else 0 for x in alpha]
+
+#make list of non-zero alphas and data points
+nz_a_l = [x for x in zip(alpha,d) if x[0] >= th]
+
+#array_np = numpy.asarray(alpha)
+#zeroind = array_np < 1e-5
+#array_np[zeroind] = 0
+#a = array_np
+#print 'a = ', a
